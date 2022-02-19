bfb67a861698b23abbcfd2bb31ea7e74ab5af937
Wei Ding <ding_web@yahoo.co.jp>
2022-02-19 00:13:24 -0800

    Initial commit

---
+++ backend/MxfAnalyzer/MxfParser.Lib/Models/VideoInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MxfParser.Lib.Models
+{
+    public class VideoInfo
+    {
+        public List<Video> Videos { get; set; }
+
+        public VideoInfo ()
+        {
+            this.Videos = new ();
+        }
+    }
+}
---
+++ backend/MxfAnalyzer/MxfParser.Lib/Services/IParser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MxfParser.Lib.Models;
+
+namespace MxfParser.Lib.Services
+{
+    public interface IParser
+    {
+        Task GetTree (string path);
+    }
+}
